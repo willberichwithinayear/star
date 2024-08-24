359be1393c0a34fc23065f0f8fffa43bcdb8728e
Robin Wohner <127152729+Robinwohner@users.noreply.github.com>
2024-08-24 14:25:16 +0900

    Update script.js
    Added firebase initialization snippet to script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDsOJ6bHZMXGJGk5ObVvhI0R7y-WX9l1YE",
+    authDomain: "project1-72725.firebaseapp.com",
+    projectId: "project1-72725",
+    storageBucket: "project1-72725.appspot.com",
+    messagingSenderId: "849993698285",
+    appId: "1:849993698285:web:af86cf38a35b48160bb5e4"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
